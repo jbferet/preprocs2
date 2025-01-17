b3fe5e4293cbe2b17abe656a44aa696517ada43b
jbferet <54911129+jbferet@users.noreply.github.com>
2025-01-17 23:02:40 +0100

    # preprocS2 v2.2.5
    ## fix
    - properly signing for planetary collections
    - deleting vignette #1

--- R/update_mask.R
+++ R/update_mask.R
@@ -26,13 +26,12 @@ update_mask <- function(aoi, collection_path, iChar, raster_dir,
   if (collection=='sentinel2-l2a-sen2lasrc'){
     item_collection <- item_collection |>
       rstactheia::items_sign_theia()
-  # } else if (collection=='sentinel-2-l2a'){
-  #   item_collection <- item_collection |>
-  #     rstac::items_sign(
-  #       rstac::sign_planetary_computer()
-  #     )
+  } else if (collection=='sentinel-2-l2a'){
+    item_collection <- item_collection |>
+      rstac::items_sign(
+        rstac::sign_planetary_computer()
+      )
   }
-
 
   # get asset name for cloud data
   asset_names <- get_cloud_asset(item_collection, collection)
