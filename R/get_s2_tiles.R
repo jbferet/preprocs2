b79f369da4ce88b653219d0f79ca34fd85574596
jbferet <54911129+jbferet@users.noreply.github.com>
2025-10-29 22:32:12 +0100

    update get_s2_tiling

--- R/get_s2_tiles.R
+++ R/get_s2_tiles.R
@@ -13,15 +13,16 @@
 
 get_s2_tiles <- function(plots, dsn_bbox, output_dir, siteName = NULL, overwrite = T,
                          path_S2tilinggrid = 'Sentinel-2_tiling_grid.kml'){
-  
+
   # add site name if provided
-  if (!is.null(siteName)) 
+  if (!is.null(siteName))
     siteName <- paste0('_', siteName)
   # define path for output files
   S2tiles_path <- file.path(output_dir,paste0('S2tiles',siteName,'.rds'))
   dsn_S2tiles_footprint <- file.path(output_dir, paste0('s2_footprint',siteName,'.gpkg'))            # path where vector is saved
   # read S2 tile info file if exists
-  if (file.exists(S2tiles_path) & overwrite == F) S2tiles <- readRDS(file = S2tiles_path)
+  if (file.exists(S2tiles_path) & overwrite == F)
+    S2tiles <- readRDS(file = S2tiles_path)
   if (!file.exists(S2tiles_path) | overwrite == T){
     # get footprint for bounding box including the plot network
     footprint <- get_s2_footprint(dsn = dsn_bbox)
@@ -29,7 +30,7 @@ get_s2_tiles <- function(plots, dsn_bbox, output_dir, siteName = NULL, overwrite
     S2tilingGrid <- sf::read_sf(path_S2tilinggrid, quiet = T)
     if (! sf::st_crs(S2tilingGrid) == sf::st_crs(footprint))
       footprint <- sf::st_transform(x = footprint, crs = sf::st_crs(S2tilingGrid))
-    
+
     S2tilingGrid$geometry <- sf::st_zm(S2tilingGrid$geometry)
     intersection <- sf::st_intersects(x = footprint, y = S2tilingGrid$geometry)
     S2tilingGridsub <- S2tilingGrid[intersection[[1]],]
@@ -46,7 +47,7 @@ get_s2_tiles <- function(plots, dsn_bbox, output_dir, siteName = NULL, overwrite
     saveRDS(object = S2tiles, file = S2tiles_path)
     # save S2 tiles intersecting with aoi as gpkg
     tilefp <- sf::st_collection_extract(x = S2tilingGridsub, type = "POLYGON")
-    sf::st_write(obj = tilefp, dsn = dsn_S2tiles_footprint, driver = 'GPKG', 
+    sf::st_write(obj = tilefp, dsn = dsn_S2tiles_footprint, driver = 'GPKG',
                  delete_dsn = T, quiet = T)
   }
   return(list('S2tiles' = S2tiles,
